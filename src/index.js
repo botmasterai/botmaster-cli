c71848d56541cc9015d85119661ae7929ede5321
John-David Wuarin <john.dwuarin@gmail.com>
2017-02-17 21:36:55 +0000

    First Commit
    Add first command the cli will handle. Namely the console bot. Still need to add support for it now.
    Should be trivial however. Simply implementing the socket.io readme and node.js's readline

---
+++ src/index.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const consoleBotClient = require('./consoleBotClient');
+
+require('yargs')
+  .command({
+    command: 'console-bot [options]',
+    aliases: ['bot', 'run', 'start-bot'],
+    desc: 'Start a console client that will connect to your socket.io botmaster bot. The connection is made to localhost:3000 by default',
+    builder: (yargs) => {
+      yargs.option('host', {
+        alias: 'h',
+        describe: 'the host your bot is on',
+        default: 'localhost',
+      });
+      yargs.option('port', {
+        alias: 'p',
+        describe: 'the port your bot is on',
+        default: 3000,
+      });
+      yargs.option('botmasterUserId', {
+        alias: ['userId', 'id', 'i'],
+        describe: 'the botmasterUserId you want to connect as. Uses socket.io\'s to generate one by default',
+      });
+    },
+    handler: (argv) => {
+      let url = `${argv.host}:${argv.port}`;
+      if (argv.botmasterUserId && typeof argv.botmasterUserId === 'string') {
+        url += `?botmasterUserId=${argv.botmasterUserId}`;
+      }
+      consoleBotClient(url);
+    },
+  })
+  .demandCommand(1)
+  .help()
+  .wrap(100)
+  .argv;
